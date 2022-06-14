43847639e4f94d564bcc5ec958696959bf29b691
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-06-14 17:05:49 +0600

    api implement

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,27 @@
+import axios from "axios";
+import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import login from "../../assets/images/login.jpg";
 const Login = () => {
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  const formHandle = async (event) => {
+    try {
+      event.preventDefault();
+      setSuccess("");
+      setError("");
+      const email = event.target.email.value;
+      const password = event.target.password.value;
+      const data = { email, password };
+      const url = "https://reqres.in/api/login";
+      const { data: result } = await axios.post(url, data);
+      if (result.token) {
+        setSuccess(result.token);
+      }
+    } catch (error) {
+      setError(error.response.data.error);
+    }
+  };
   return (
     <Container className="mt-3">
       <Row className="justify-item-center">
@@ -9,18 +30,31 @@ const Login = () => {
             <div>
               <h2 className="text-center">Welcome Back</h2>
               <p className="text-center">Please Login ATools </p>
-              <Form>
+              <Form onSubmit={formHandle}>
                 <Form.Group className="mb-3" controlId="email">
-                  <Form.Control type="email" placeholder="Email Address*" />
+                  <Form.Control
+                    name="email"
+                    type="email"
+                    placeholder="Email Address*"
+                  />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="password">
-                  <Form.Control type="password" placeholder="Password*" />
+                  <Form.Control
+                    name="password"
+                    type="password"
+                    placeholder="Password*"
+                  />
                 </Form.Group>
 
                 <Button variant="dark" className="d-block w-100" type="submit">
                   Login
                 </Button>
               </Form>
+              {success && (
+                <p className="text-success mt-2"> Token : {success}</p>
+              )}
+              {error && <p className="text-danger mt-2"> Error : {error}</p>}
+
               <div className="d-flex justify-content-between  flex-wrap mt-2">
                 <Form.Check
                   className="pe-3"
